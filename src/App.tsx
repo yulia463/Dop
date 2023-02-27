fcf62a46b0b906087d4cdf5e84aaf9082eee333b
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-02-27 15:06:35 +0100

    add  and refactoring ShopList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.module.css';
+import {ShopList} from "./ShopList";
+import {ButtonsFilterType, FilterType, ThingsToBuyPropsType} from "./Typisation";
+import {v1} from "uuid";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    const [thingsToBuy, setThingsToBuy] = useState<ThingsToBuyPropsType[]>([
+        {id: v1(), title: 'Milk', expectedPrice: '$1.99', realPrice: '$1.99', inCart: true},
+        {id: v1(), title: 'Bread', expectedPrice: '$0.99', realPrice: '$0.89', inCart: true},
+        {id: v1(), title: 'Coca-Cola', expectedPrice: '$1.49', realPrice: '$1.49', inCart: true},
+        {id: v1(), title: 'Eggs', expectedPrice: '$2.49', realPrice: '$3.99', inCart: false},
+        {id: v1(), title: 'Cakes', expectedPrice: '$4.99', realPrice: '$6.99', inCart: false},
+    ])
+
+const buttons :ButtonsFilterType[]=[
+    {title:"all",filterButtonValue:'all'},
+    {title:"buy",filterButtonValue:'buy'},
+    {title:"not buy",filterButtonValue:'not buy'}
+]
+
+    const changeCardStatus = (itemId: string, inCartStatus: boolean) => {
+        const thingsToChange = thingsToBuy.find((el) =>  el.id === itemId)
+        if (thingsToChange){
+            thingsToChange.inCart = inCartStatus
+            setThingsToBuy([...thingsToBuy])
+        }
+    }
+
+    const [filter, setFilter] = useState<FilterType>("all")
+    const deleteItemShop = (id:string) => {
+        let things = thingsToBuy.filter(th => th.id !== id)
+        setThingsToBuy(things)
+    }
+    let thingsToShopList = filter === "buy"
+        ? thingsToBuy.filter(el=>el.inCart === true)
+        : filter === "not buy"
+            ? thingsToBuy.filter(el=>el.inCart === false)
+            : thingsToBuy
+
+    const changeFilter = (newFilterValue:FilterType)=>{
+        setFilter(newFilterValue)
+    }
+    const addtask =(newTitle:string)=>{
+        const newTavar={id: v1(), title: newTitle, expectedPrice: '$4.99', realPrice: '$6.99', inCart: false}
+        setThingsToBuy([newTavar,...thingsToBuy])
+
+    }
+
+    return (
+        <div className="App">
+            <ShopList
+                title = {"What to buy"}
+                whatToBuy = {thingsToShopList}
+                deleteItemShop = {deleteItemShop}
+                changeFilter={changeFilter}
+                addtask={addtask}
+                changeCardStatus={changeCardStatus}
+                filter={filter}
+                buttonsFilter={buttons}
+            />
+        </div>
+    );
+}
 export default App;
